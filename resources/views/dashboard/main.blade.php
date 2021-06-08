fed24fd740e77e683f24b0923945a7f39870e568
alvian21 <alvian.ardhiansyah@gmail.com>
2021-06-08 23:10:22 +0700

    revisi

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -53,19 +53,19 @@
 
     <div class="main-container">
         @yield('content')
-	</div>
-	<!-- js -->
-	<script src="{{asset('assets/vendors/scripts/core.js')}}"></script>
-	<script src="{{asset('assets/vendors/scripts/script.min.js')}}"></script>
-	<script src="{{asset('assets/vendors/scripts/process.js')}}"></script>
-	<script src="{{asset('assets/vendors/scripts/layout-settings.js')}}"></script>
-	{{-- <script src="{{asset('assets/src/plugins/apexcharts/apexcharts.min.js')}}"></script> --}}
-	<script src="{{asset('assets/src/plugins/datatables/js/jquery.dataTables.min.js')}}"></script>
-	<script src="{{asset('assets/src/plugins/datatables/js/dataTables.bootstrap4.min.js')}}"></script>
-	<script src="{{asset('assets/src/plugins/datatables/js/dataTables.responsive.min.js')}}"></script>
-	<script src="{{asset('assets/src/plugins/datatables/js/responsive.bootstrap4.min.js')}}"></script>
-	{{-- <script src="{{asset('assets/vendors/scripts/dashboard.js')}}"></script> --}}
-    <script src="{{asset('assets/src/scripts/sweetalert2.min.js')}}"></script>
+    </div>
+    <!-- js -->
+    <script src="{{asset('assets/vendors/scripts/core.js')}}"></script>
+    <script src="{{asset('assets/vendors/scripts/script.min.js')}}"></script>
+    <script src="{{asset('assets/vendors/scripts/process.js')}}"></script>
+    <script src="{{asset('assets/vendors/scripts/layout-settings.js')}}"></script>
+    <script src="{{asset('assets/src/plugins/apexcharts/apexcharts.min.js')}}"></script>
+    <script src="{{asset('assets/src/plugins/datatables/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('assets/src/plugins/datatables/js/dataTables.bootstrap4.min.js')}}"></script>
+    <script src="{{asset('assets/src/plugins/datatables/js/dataTables.responsive.min.js')}}"></script>
+    <script src="{{asset('assets/src/plugins/datatables/js/responsive.bootstrap4.min.js')}}"></script>
+    <script src="{{asset('assets/vendors/scripts/dashboard.js')}}"></script>
+    <script src="{{asset('assets/src/scripts/sweetalert2.all.min.js')}}"></script>
     @stack('script')
 </body>
 
